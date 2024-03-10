3d52b504446188eb4a9ccda5e3d408145e9c686e
Sher Khan Baloch <113584739+sherkhanbaloch@users.noreply.github.com>
2024-03-10 13:42:41 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('#typed', {
+    strings: ['Desktop Applications Developer', 'Web Applications Developer', 'CS Undergrad', 'Freelancer'],
+    backSpeed: 50,
+    typeSpeed: 50,
+    smartBackspace: true,
+    loop: true
+});
