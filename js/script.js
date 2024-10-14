a93d44161edf8b711533ccc8fc48fafad8e89cf9
Sher Khan Baloch <113584739+sherkhanbaloch@users.noreply.github.com>
2024-10-14 21:56:17 +0500

    updated about me image and worked in portfolio cards

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('#typed', {
-    strings: ['Desktop Applications Developer', 'Web Applications Developer', 'CS Undergrad', 'Freelancer'],
+    strings: ['Desktop Application Developer', 'Web Application Developer', 'CS Undergrad', 'Freelancer'],
     backSpeed: 50,
     typeSpeed: 50,
     smartBackspace: true,
